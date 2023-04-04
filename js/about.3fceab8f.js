326e33124ad2c8573857dbd6f9df981f02551877
Limdongsoo <101685800+Limdongsoo@users.noreply.github.com>
2023-04-04 10:03:58 +0900

    Delete js directory

--- js/about.3fceab8f.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkshop"]=self["webpackChunkshop"]||[]).push([[443],{7381:function(n,s,u){u.r(s),u.d(s,{default:function(){return f}});var t=u(3396);const a={class:"about"},e=(0,t._)("h1",null,"This is an about page",-1),r=[e];function c(n,s){return(0,t.wg)(),(0,t.iD)("div",a,r)}var o=u(89);const i={},h=(0,o.Z)(i,[["render",c]]);var f=h}}]);
-//# sourceMappingURL=about.3fceab8f.js.map
